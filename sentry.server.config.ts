9f8cef31a3e417bb13a3a6aad5cfbe72663d888f
Ratna Amalia Azzahra <112413422+amaliartnaa@users.noreply.github.com>
2025-06-07 17:15:13 +0700

    feat: setup sentry release & sourcemap upload

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://253a61e04b186983b02b6eb0a628cedb@o4509442225537024.ingest.us.sentry.io/4509456488202240",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
