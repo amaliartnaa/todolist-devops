2df2876fd7e99e309f2a4b34c115b1917272f61c
amaliartnaa <amaliaratna41@gmail.com>
2025-06-15 19:37:42 +0700

    ci: modify workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function HomePage() {
 
   return (
     <main className="mx-auto max-w-xl space-y-6 px-4 py-10">
-      <h1 className="text-center text-2xl font-bold">Todo List coba📝</h1>
+      <h1 className="text-center text-2xl font-bold">Todo List 📝</h1>
 
       <TodoForm onAdd={handleAdd} />
       <Divider className="my-4" />
