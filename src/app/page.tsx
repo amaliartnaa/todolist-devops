b59540020d354a0dee0b3887869449338574528c
Hanin <hfz.haninnuha@gmail.com>
2025-06-15 14:44:36 +0700

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function HomePage() {
 
   return (
     <main className="mx-auto max-w-xl space-y-6 px-4 py-10">
-      <h1 className="text-center text-2xl font-bold">Todo List test📝</h1>
+      <h1 className="text-center text-2xl font-bold">Todo List testTEST📝</h1>
 
       <TodoForm onAdd={handleAdd} />
       <Divider className="my-4" />
