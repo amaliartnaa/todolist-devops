40d17ef392d728481d321b90aca593cfa7b86d1f
amaliartnaa <amaliaratna41@gmail.com>
2025-06-16 23:55:35 +0700

    fix: modify cors origin

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:3000",
       "https://todo-app-381607765507.asia-southeast2.run.app",
       "https://todo-app-dev-381607765507.asia-southeast2.run.app",
+      "https://todo-backend-dev-kjeipxceka-et.a.run.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
