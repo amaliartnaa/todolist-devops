43fa5e121f654dce87e3f8ce9f806d31c9db141c
amaliartnaa <amaliaratna41@gmail.com>
2025-06-16 23:29:21 +0700

    fix: modify cors allowed headers

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -17,7 +17,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
