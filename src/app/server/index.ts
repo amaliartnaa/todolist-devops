6529f44099d2c69aec7122b7d3999f8be691f16d
amaliartnaa <amaliaratna41@gmail.com>
2025-06-16 13:48:36 +0700

    fix: modify cors origin for frontend

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "http://localhost:3000",
+      "https://todo-app-381607765507.asia-southeast2.run.app",
+      "https://todo-app-dev-381607765507.asia-southeast2.run.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
