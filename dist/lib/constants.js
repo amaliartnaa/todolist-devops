82f81559196d69f0d52ef56f4c215261d6687212
amaliartnaa <amaliaratna41@gmail.com>
2025-06-18 00:57:17 +0700

    fix: modify dist

---
+++ dist/lib/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.priorityOrder = exports.priorities = exports.categories = void 0;
+exports.categories = ["Work", "Personal", "Study", "Other"];
+exports.priorities = ["Low", "Medium", "High"];
+exports.priorityOrder = {
+    Low: 1,
+    Medium: 2,
+    High: 3,
+};
